ba909bfe0d02961f669558aeebb148a6da078d60
Benjamin Yee <benjaminyee@MacBookPro.attlocal.net>
2025-01-03 01:06:15 -0800

    Add persistence.nvim and trouble.nvim plugins. Add more themes.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -343,6 +343,8 @@ require('lazy').setup({
         { '<leader>h', group = '[H]arpoon' },
         { '<leader>g', group = '[G]itsigns' },
         { '<leader>a', group = '[A]uxillary Commands' },
+        { '<leader>x', group = '[X] Trouble' },
+        { '<leader>1', group = '[1] Persistence (Sessions)' },
       },
     },
   },
@@ -439,6 +441,9 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      -- NOTE: I added...
+      vim.keymap.set('n', '<leader>st', builtin.treesitter, { desc = '[S]earch [T]reesitter' })
+      vim.keymap.set('n', '<leader>at', builtin.colorscheme, { desc = '[T]hemes Telescope' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
@@ -904,10 +909,27 @@ require('lazy').setup({
     'Mofiqul/dracula.nvim',
     priority = 1000,
     init = function()
-      vim.cmd.colorscheme 'dracula'
+      -- vim.cmd.colorscheme 'dracula'
     end,
   },
-
+  {
+    'eldritch-theme/eldritch.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    init = function()
+      vim.cmd.colorscheme 'eldritch'
+    end,
+  },
+  {
+    'marko-cerovac/material.nvim',
+    priority = 1000,
+    init = function()
+      -- vim.g.material_style = 'deep ocean' -- oceanic, deep ocean, palenight, lighter, darker
+      -- vim.cmd.colorscheme 'material'
+    end,
+  },
+  { 'miikanissi/modus-themes.nvim', priority = 1000 },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
--- .config/nvim/lua/keybindings/keybindings.lua
+++ .config/nvim/lua/keybindings/keybindings.lua
@@ -8,3 +8,29 @@ vim.keymap.set('n', '<leader>hx', require('harpoon.mark').add_file, { desc = '[H
 vim.keymap.set('n', '<leader>hn', require('harpoon.ui').nav_next, { desc = '[H]arpoon: [N]ext mark' })
 vim.keymap.set('n', '<leader>hp', require('harpoon.ui').nav_prev, { desc = '[H]arpoon: [P]revious mark' })
 vim.keymap.set('n', '<leader>hm', ':Telescope harpoon marks<CR>', { desc = '[H]arpoon [M]arks view' })
+
+-- Persistence keybindings
+-- -- load the session for the current directory
+vim.keymap.set('n', '<leader>1s', function()
+  require('persistence').load()
+end, { desc = 'Load Session for Current Directory' })
+
+-- select a session to load
+vim.keymap.set('n', '<leader>1S', function()
+  require('persistence').select()
+end, { desc = 'Select Session to Load' })
+
+-- load the last session
+vim.keymap.set('n', '<leader>1l', function()
+  require('persistence').load { last = true }
+end, { desc = 'Load Last Session' })
+
+-- stop Persistence => session won't be saved on exit
+vim.keymap.set('n', '<leader>1d', function()
+  require('persistence').stop()
+end, { desc = "Stop Persistence. (Session won't be saved on exit)" })
+
+-- Themery
+-- vim.keymap.set('n', '<leader>at', function()
+--   vim.cmd
+-- end, { desc = '[T]hemery' })
