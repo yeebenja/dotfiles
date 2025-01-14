4b49f695e52afaff1a964740e3b883b476d3508d
Benjamin Yee <benjaminyee@Bens-MacBook-9.local>
2025-01-13 19:46:27 -0500

    modify incline

--- .config/nvim/lua/custom/plugins/incline.lua
+++ .config/nvim/lua/custom/plugins/incline.lua
@@ -30,11 +30,30 @@ return {
         unlisted_buffers = true,
         wintypes = 'special',
       },
-      render = 'basic',
+      -- render = 'basic',
+      render = function(props)
+        local helpers = require 'incline.helpers'
+        local devicons = require 'nvim-web-devicons'
+        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+        if filename == '' then
+          filename = '[No Name]'
+        end
+        local ft_icon, ft_color = devicons.get_icon_color(filename)
+        local modified = vim.bo[props.buf].modified
+        return {
+          ft_icon and { ' ', ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or '',
+          ' ',
+          { filename, gui = modified and 'bold,italic' or 'bold' },
+          ' ',
+          guibg = '#44406e',
+        }
+      end,
       window = {
         margin = {
           horizontal = 1,
           vertical = 1,
+          -- horizontal = 0,
+          -- vertical = 0,
         },
         options = {
           signcolumn = 'no',
