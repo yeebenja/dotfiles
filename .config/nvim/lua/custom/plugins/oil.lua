63301c0d80f1561985cb13b80c043d344253d46a
Benjamin Yee <benjaminyee@Bens-MacBook-9.local>
2025-01-19 13:50:34 -0500

    modified oil.lua to not include size and modified time

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -11,8 +11,8 @@ return {
       columns = {
         'icon',
         -- "permissions",
-        'size',
-        'mtime',
+        -- 'size',
+        -- 'mtime',
       },
       -- Buffer-local options to use for oil buffers
       buf_options = {
