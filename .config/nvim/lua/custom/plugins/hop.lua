c95febfe5f49e643f23ff416833b8100c5d30050
Benjamin Yee <benjaminyee@Bens-MacBook-9.local>
2025-01-20 14:22:20 -0500

    update plugin commits via LazyUpdate

--- .config/nvim/lua/custom/plugins/hop.lua
+++ .config/nvim/lua/custom/plugins/hop.lua
@@ -1,18 +1,9 @@
--- -- ~/.config/nvim/lua/plugins/treesitter.lua
--- return {
---     "nvim-treesitter/nvim-treesitter",
---     build = ":TSUpdate",
---     config = function()
---         require("nvim-treesitter.configs").setup({
---             highlight = { enable = true },
---             indent = { enable = true },
---         })
---     end,
--- }
 return {
   'smoka7/hop.nvim',
   version = '*',
   config = function()
-    require('hop').setup { keys = 'asdghklqwertyuiopzxcvbnmfj' }
+    require('hop').setup {
+      keys = 'asdghklqwertyuiopzxcvbnmfj',
+    }
   end,
 }
