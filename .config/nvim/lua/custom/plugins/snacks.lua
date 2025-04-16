c3e4f73cc701acbd90a4a986b26b385cfd0cb2d0
Benjamin Yee <benjaminyee@Bens-MacBook.local>
2025-04-16 14:18:15 -0400

    Add snacks picker

--- .config/nvim/lua/custom/plugins/snacks.lua
+++ .config/nvim/lua/custom/plugins/snacks.lua
@@ -12,10 +12,220 @@ return {
     -- indent = { enabled = true },
     -- input = { enabled = true },
     notifier = { enabled = true },
+    picker = {
+      -- NOTE: Everything in the picker opts is from linkarzu
+      -- Here is the link: https://github.com/linkarzu/dotfiles-latest/blob/main/neovim/neobean/lua/plugins/snacks.lua
+      --
+      -- Documentation for the picker
+      -- https://github.com/folke/snacks.nvim/blob/main/docs/picker.md
+      -- My ~/github/dotfiles-latest/neovim/lazyvim/lua/config/keymaps.lua
+      -- file was always showing at the top, I needed a way to decrease its
+      -- score, in frecency you could use :FrecencyDelete to delete a file
+      -- from the database, here you can decrease it's score
+      transform = function(item)
+        if not item.file then
+          return item
+        end
+        -- Demote the "lazyvim" keymaps file:
+        if item.file:match 'lazyvim/lua/config/keymaps%.lua' then
+          item.score_add = (item.score_add or 0) - 30
+        end
+        -- Boost the "neobean" keymaps file:
+        -- if item.file:match("neobean/lua/config/keymaps%.lua") then
+        --   item.score_add = (item.score_add or 0) + 100
+        -- end
+        return item
+      end,
+      -- In case you want to make sure that the score manipulation above works
+      -- or if you want to check the score of each file
+      debug = {
+        scores = false, -- show scores in the list
+      },
+      -- I like the "ivy" layout, so I set it as the default globaly, you can
+      -- still override it in different keymaps
+      layout = {
+        preset = 'ivy',
+        -- When reaching the bottom of the results in the picker, I don't want
+        -- it to cycle and go back to the top
+        cycle = false,
+      },
+      layouts = {
+        -- I wanted to modify the ivy layout height and preview pane width,
+        -- this is the only way I was able to do it
+        -- NOTE: I don't think this is the right way as I'm declaring all the
+        -- other values below, if you know a better way, let me know
+        --
+        -- Then call this layout in the keymaps above
+        -- got example from here
+        -- https://github.com/folke/snacks.nvim/discussions/468
+        ivy = {
+          layout = {
+            box = 'vertical',
+            backdrop = false,
+            row = -1,
+            width = 0,
+            height = 0.5,
+            border = 'top',
+            title = ' {title} {live} {flags}',
+            title_pos = 'left',
+            { win = 'input', height = 1, border = 'bottom' },
+            {
+              box = 'horizontal',
+              { win = 'list', border = 'none' },
+              { win = 'preview', title = '{preview}', width = 0.5, border = 'left' },
+            },
+          },
+        },
+        -- I wanted to modify the layout width
+        --
+        vertical = {
+          layout = {
+            backdrop = false,
+            width = 0.8,
+            min_width = 80,
+            height = 0.8,
+            min_height = 30,
+            box = 'vertical',
+            border = 'rounded',
+            title = '{title} {live} {flags}',
+            title_pos = 'center',
+            { win = 'input', height = 1, border = 'bottom' },
+            { win = 'list', border = 'none' },
+            { win = 'preview', title = '{preview}', height = 0.4, border = 'top' },
+          },
+        },
+      },
+      matcher = {
+        fuzzy = true, -- use fuzzy matching
+        smartcase = true, -- use smartcase
+        ignorecase = true, -- use ignorecase
+        sort_empty = false, -- sort results when the search string is empty
+        filename_bonus = true, -- give bonus for matching file names (last part of the path)
+        file_pos = true, -- support patterns like `file:line:col` and `file:line`
+        -- the bonusses below, possibly require string concatenation and path normalization,
+        -- so this can have a performance impact for large lists and increase memory usage
+        cwd_bonus = false, -- give bonus for matching files in the cwd
+        frecency = true, -- frecency bonus
+        history_bonus = true, -- give more weight to chronological order
+      },
+      sort = {
+        -- default sort is by score, text length and index
+        fields = { 'score:desc', '#text', 'idx' },
+      },
+      win = {
+        input = {
+          keys = {
+            -- to close the picker on ESC instead of going to normal mode,
+            -- add the following keymap to your config
+            ['<Esc>'] = { 'close', mode = { 'n', 'i' } },
+            -- I'm used to scrolling like this in LazyGit
+            ['J'] = { 'preview_scroll_down', mode = { 'i', 'n' } },
+            ['K'] = { 'preview_scroll_up', mode = { 'i', 'n' } },
+            ['H'] = { 'preview_scroll_left', mode = { 'i', 'n' } },
+            ['L'] = { 'preview_scroll_right', mode = { 'i', 'n' } },
+          },
+        },
+      },
+      formatters = {
+        file = {
+          filename_first = true, -- display filename before the file path
+          truncate = 80,
+        },
+      },
+    },
     -- quickfile = { enabled = true },
     -- scroll = { enabled = true },
     -- statuscolumn = { enabled = true },
     -- words = { enabled = true },
     -- terminal = { enabled = true },
+  },
+  keys = {
+    -- Git Log
+    {
+      '<leader>gl',
+      function()
+        Snacks.picker.git_log {
+          finder = 'git_log',
+          format = 'git_log',
+          preview = 'git_show',
+          confirm = 'git_checkout',
+          layout = 'vertical',
+        }
+      end,
+      desc = 'Git [L]og',
+    },
+    -- Git Branches
+    {
+      '<leader>gc',
+      function()
+        Snacks.picker.git_branches {
+          layout = 'select',
+        }
+      end,
+      desc = '[C]heckout Branches',
+    },
+    -- Git Status
+    {
+      '<leader>gt',
+      function()
+        Snacks.picker.git_status {
+          layout = 'vertical',
+        }
+      end,
+      desc = 'Git S[T]atus',
+    },
+    -- Search Files
+    {
+      '<leader>sf',
+      function()
+        Snacks.picker.files {
+          finder = 'files',
+          format = 'file',
+          show_empty = true,
+          supports_live = true,
+          layout = 'ivy',
+        }
+      end,
+      desc = '[S]earch [F]iles',
+    },
+    -- Search Keymaps
+    {
+      '<leader>sk',
+      function()
+        Snacks.picker.keymaps {
+          layout = 'vertical',
+        }
+      end,
+      desc = '[S]earch [K]eymaps',
+    },
+    -- Buffer Picker
+    {
+      '<leader><leader>',
+      function()
+        Snacks.picker.buffers {
+          -- I always want my buffers picker to start in normal mode
+          on_show = function()
+            vim.cmd.stopinsert()
+          end,
+          finder = 'buffers',
+          format = 'buffer',
+          hidden = false,
+          unloaded = true,
+          current = true,
+          sort_lastused = true,
+          win = {
+            input = {
+              keys = {
+                ['d'] = 'bufdelete',
+              },
+            },
+            list = { keys = { ['d'] = 'bufdelete' } },
+          },
+          -- In case you want to override the layout for this keymap
+          -- layout = "ivy",
+        }
+      end,
+      desc = 'Buffer Picker',
+    },
   },
 }
