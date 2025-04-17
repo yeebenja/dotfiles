1c337ca386c42d90f79dabae5972b4dfb0627481
Benjamin Yee <benjaminyee@0587439266.wireless.umich.net>
2025-04-17 15:33:25 -0400

    add comments under telescope frequency

--- .config/nvim/lua/custom/plugins/telescope-frecency.lua
+++ .config/nvim/lua/custom/plugins/telescope-frecency.lua
@@ -6,7 +6,13 @@ return {
     require('telescope').load_extension 'frecency'
   end,
 }
-
+-- You can delete entries from DB by this command. This command does not remove the file itself, only from DB.
+-- - delete the current opened file
+-- - :FrecencyDelete
+-- - delete the supplied path-
+-- - :FrecencyDelete /full/path/to/the/file
+--
+--]===]
 -- watch this video
 -- https://www.youtube.com/watch?v=Qr-vX51gB8g
 --
