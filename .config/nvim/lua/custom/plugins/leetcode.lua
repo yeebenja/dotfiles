362046f7ceb01291888700b48b634e22ff16500c
Benjamin Yee <yeebenja@outlook.com>
2025-11-07 12:10:20 -0600

    add image.nvim and modify leetcode.nvim

--- .config/nvim/lua/custom/plugins/leetcode.lua
+++ .config/nvim/lua/custom/plugins/leetcode.lua
@@ -8,5 +8,100 @@ return {
   },
   opts = {
     -- configuration goes here
+    ---@type string
+    arg = 'leetcode.nvim',
+
+    ---@type lc.lang
+    lang = 'python3',
+
+    cn = { -- leetcode.cn
+      enabled = false, ---@type boolean
+      translator = true, ---@type boolean
+      translate_problems = true, ---@type boolean
+    },
+
+    ---@type lc.storage
+    storage = {
+      home = vim.fn.stdpath 'data' .. '/leetcode',
+      cache = vim.fn.stdpath 'cache' .. '/leetcode',
+    },
+
+    ---@type table<string, boolean>
+    plugins = {
+      non_standalone = false,
+    },
+
+    ---@type boolean
+    logging = true,
+
+    injector = {}, ---@type table<lc.lang, lc.inject>
+
+    cache = {
+      update_interval = 60 * 60 * 24 * 7, ---@type integer 7 days
+    },
+
+    editor = {
+      reset_previous_code = true, ---@type boolean
+      fold_imports = true, ---@type boolean
+    },
+
+    console = {
+      open_on_runcode = true, ---@type boolean
+
+      dir = 'row', ---@type lc.direction
+
+      size = { ---@type lc.size
+        width = '70%', -- default '90%'
+        height = '50%', -- default '75%'
+      },
+
+      result = {
+        size = '60%', ---@type lc.size
+      },
+
+      testcase = {
+        virt_text = true, ---@type boolean
+
+        size = '40%', ---@type lc.size
+      },
+    },
+
+    description = {
+      position = 'left', ---@type lc.position
+
+      width = '40%', ---@type lc.size
+
+      show_stats = true, ---@type boolean
+    },
+
+    ---@type lc.picker
+    picker = { provider = nil },
+
+    hooks = {
+      ---@type fun()[]
+      ['enter'] = {},
+
+      ---@type fun(question: lc.ui.Question)[]
+      ['question_enter'] = {},
+
+      ---@type fun()[]
+      ['leave'] = {},
+    },
+
+    keys = {
+      toggle = { 'q' }, ---@type string|string[]
+      confirm = { '<CR>' }, ---@type string|string[]
+
+      reset_testcases = 'r', ---@type string
+      use_testcase = 'U', ---@type string
+      focus_testcases = 'H', ---@type string
+      focus_result = 'L', ---@type string
+    },
+
+    ---@type lc.highlights
+    theme = {},
+
+    ---@type boolean
+    image_support = true,
   },
 }
