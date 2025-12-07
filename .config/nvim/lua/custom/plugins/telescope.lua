835f4b0246fae8b1d01f84e17b910490e9f66c28
Benjamin Yee <yeebenja@outlook.com>
2025-12-07 14:33:37 -0600

    change telescope "search help" to Snacks "search help"

--- .config/nvim/lua/custom/plugins/telescope.lua
+++ .config/nvim/lua/custom/plugins/telescope.lua
@@ -81,7 +81,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+    -- vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     -- vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
     -- vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
     -- "<leader>ff",
