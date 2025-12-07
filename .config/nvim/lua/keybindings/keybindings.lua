3117e8081a84873feffe2c446b5276530c5b4a6c
Benjamin Yee <benjaminyee@Bens-MacBook.local>
2025-12-07 14:00:21 -0600

    disable lsp-lines and add tiny-inline-diagnostic

--- .config/nvim/lua/keybindings/keybindings.lua
+++ .config/nvim/lua/keybindings/keybindings.lua
@@ -161,20 +161,27 @@ end, { desc = 'Toggle Cursor Center' })
 --   desc = 'Search on current file',
 -- })
 
--- lsp-lines toggle
-vim.keymap.set('n', '<leader>a3', function()
-  local config = vim.diagnostic.config()
-  local is_lines_enabled = config.virtual_lines
+-- DISABLED lsp-lines toggle
+-- vim.keymap.set('n', '<leader>a3', function()
+--   local config = vim.diagnostic.config()
+--   local is_lines_enabled = config.virtual_lines
+--
+--   if is_lines_enabled then
+--     Snacks.notifier.notify('LSP Lines Disabled', 'info', { style = 'compact', timeout = 2000, title = 'Snacks Notifier' })
+--     vim.diagnostic.config { virtual_lines = false, virtual_text = true }
+--   else
+--     Snacks.notifier.notify('LSP Lines Enabled', 'info', { style = 'compact', timeout = 2000, title = 'Snacks Notifier' })
+--     vim.diagnostic.config { virtual_lines = true, virtual_text = false }
+--   end
+-- end, {
+--   desc = 'LSP Lines Toggle',
+-- })
 
-  if is_lines_enabled then
-    Snacks.notifier.notify('LSP Lines Disabled', 'info', { style = 'compact', timeout = 2000, title = 'Snacks Notifier' })
-    vim.diagnostic.config { virtual_lines = false, virtual_text = true }
-  else
-    Snacks.notifier.notify('LSP Lines Enabled', 'info', { style = 'compact', timeout = 2000, title = 'Snacks Notifier' })
-    vim.diagnostic.config { virtual_lines = true, virtual_text = false }
-  end
+-- tiny inclinde diagnostics
+vim.keymap.set('n', '<leader>a3', function()
+  vim.cmd 'TinyInlineDiag toggle'
 end, {
-  desc = 'LSP Lines Toggle',
+  desc = 'Tiny Incline Diagnostics Toggle',
 })
 
 -- Up and Down (my own custon keymappings)
