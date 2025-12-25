4eeccdaa358130db33ae15c2203ed69f11ce278f
Benjamin Yee <benjaminyee@MacBookPro.attlocal.net>
2025-12-25 00:24:46 -0800

    modify nvim-ufo config to include # of lines folded and modify column statusline

--- .config/nvim/lua/nvim-ufo-config/nvim-fold-config.lua
+++ .config/nvim/lua/nvim-ufo-config/nvim-fold-config.lua
@@ -1,17 +1,35 @@
 vim.o.foldcolumn = '1' -- '0' is not bad
-vim.o.foldlevel = 99
--- - Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
-vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = 'UFO: Open all folds' })
-vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = 'UFO: Close all folds' })
-vim.keymap.set('n', 'zK', function()
-  local winid = require('ufo').peekFoldedLinesUnderCursor()
-  if not winid then
-    vim.lsp.buf.hover()
+local handler = function(virtText, lnum, endLnum, width, truncate)
+  local newVirtText = {}
+  local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+  local sufWidth = vim.fn.strdisplaywidth(suffix)
+  local targetWidth = width - sufWidth
+  local curWidth = 0
+  for _, chunk in ipairs(virtText) do
+    local chunkText = chunk[1]
+    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+    if targetWidth > curWidth + chunkWidth then
+      table.insert(newVirtText, chunk)
+    else
+      chunkText = truncate(chunkText, targetWidth - curWidth)
+      local hlGroup = chunk[2]
+      table.insert(newVirtText, { chunkText, hlGroup })
+      chunkWidth = vim.fn.strdisplaywidth(chunkText)
+      -- str width returned from truncate() may less than 2nd argument, need padding
+      if curWidth + chunkWidth < targetWidth then
+        suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+      end
+      break
+    end
+    curWidth = curWidth + chunkWidth
   end
-end, { desc = 'UFO: Peek Fold' })
+  table.insert(newVirtText, { suffix, 'MoreMsg' })
+  return newVirtText
+end
 
 -- NOTE: breaking changes here
 -- Option 2: nvim lsp as LSP client
@@ -29,4 +47,63 @@ for _, ls in ipairs(language_servers) do
     -- you can add other fields for setting up lsp server in this table
   }
 end
-require('ufo').setup()
+local ftMap = {
+  vim = 'indent',
+  python = { 'indent' },
+  git = '',
+}
+require('ufo').setup {
+
+  fold_virt_text_handler = handler,
+  open_fold_hl_timeout = 150,
+  close_fold_kinds_for_ft = {
+    default = { 'imports', 'comment' },
+    json = { 'array' },
+    c = { 'comment', 'region' },
+  },
+  close_fold_current_line_for_ft = {
+    default = true,
+    c = false,
+  },
+  preview = {
+    win_config = {
+      border = { '', '─', '', '', '', '─', '', '' },
+      winhighlight = 'Normal:Folded',
+      winblend = 0,
+    },
+    mappings = {
+      scrollU = '<C-u>',
+      scrollD = '<C-d>',
+      jumpTop = '[',
+      jumpBot = ']',
+    },
+  },
+  provider_selector = function(bufnr, filetype, buftype)
+    -- if you prefer treesitter provider rather than lsp,
+    -- return ftMap[filetype] or {'treesitter', 'indent'}
+    return ftMap[filetype]
+
+    -- refer to ./doc/example.lua for detail
+  end,
+}
+-- keymaps for ufo
+vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = 'UFO: Open all folds' })
+vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = 'UFO: Close all folds' })
+vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
+vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+vim.keymap.set('n', 'zK', function()
+  local winid = require('ufo').peekFoldedLinesUnderCursor()
+  if not winid then
+    vim.lsp.buf.hover()
+  end
+end, { desc = 'UFO: Peek Fold' })
+
+-- custom symbols for column status line
+vim.opt.fillchars = {
+  foldopen = '▼',
+  foldclose = '▶',
+  fold = ' ',
+  foldsep = ' ',
+  diff = '╱',
+  eob = ' ',
+}
