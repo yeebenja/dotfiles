fbb266f69821005c09a5a81143d440c8cb907ddb
Benjamin Yee <benjaminyee@MacBookPro.attlocal.net>
2025-12-25 00:24:07 -0800

    modify lualine to include current git worktree

--- .config/nvim/lua/custom/plugins/lualine.lua
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -1,4 +1,26 @@
+-- EFFECTS: fetches worktree devicon symbol and current git worktree
+local function git_worktree_with_icon()
+  local handle = io.popen 'git rev-parse --show-toplevel 2>/dev/null'
+  if not handle then
+    return ''
+  end
+
+  local result = handle:read '*a'
+  handle:close()
+
+  if not result or result == '' then
+    return ''
+  end
+
+  local worktree = result:gsub('\n', '')
+  local name = worktree:match '[^/]+$'
+  local icon = '󱁕' -- (git worktree icon)
+
+  return string.format('%s %s', icon, name)
+end
+
 return {
+
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
@@ -18,26 +40,46 @@ return {
       statusline = 100,
       tabline = 100,
       winbar = 100,
+      refresh_time = 16, -- ~60fps
     },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = {
+        'branch',
+        {
+          git_worktree_with_icon,
+          cond = function()
+            return vim.fn.isdirectory '.git' == 1 or vim.fn.system('git rev-parse --git-dir 2>/dev/null'):find '.git' ~= nil
+          end,
+        },
+        'diff',
+        'diagnostics',
+      },
+      lualine_c = {
+        'filename',
+        -- -- date time
+        -- {
+        --   function()
+        --     return os.date '%H:%M:%S %a %m/%d/%Y'
+        --   end,
+        -- }, -- 14:30 12/24/2024
+        -- Git worktree
+      },
+      lualine_x = { 'encoding', 'fileformat', 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { 'filename' },
+      lualine_x = { 'location' },
+      lualine_y = {},
+      lualine_z = {},
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {},
   },
-  sections = {
-    lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
-    lualine_x = { 'encoding', 'fileformat', 'filetype' },
-    lualine_y = { 'progress' },
-    lualine_z = { 'location' },
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { 'filename' },
-    lualine_x = { 'location' },
-    lualine_y = {},
-    lualine_z = {},
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {},
 }
