da043599725950b66557234609aa68a6a011f0c0
Benjamin Yee <benjaminyee@Bens-MacBook.local>
2025-12-26 19:43:47 -0800

    add global colors

--- .config/nvim/lua/globals/colors.lua
+++ .config/nvim/lua/globals/colors.lua
@@ -3,6 +3,9 @@ local C = {}
 
 C.colors = {
   modified_buffer_color = '#ff6b6b',
+  eldritch_red = '#f364b5', -- eldritch red
+  fg_original = '#1a1c2c', -- original foreground yank highlight
+  bg_original = '#f7c67f', -- original background yank highlight
 }
 
 return C
