844a6f7d4d82e359309ffded932d29462a90255c
Benjamin Yee <yeebenja@outlook.com>
2025-12-26 19:43:47 -0800

    add global colors

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -249,7 +249,7 @@ vim.keymap.del('x', 'gra')
 -- NOTE: since leap.nvim uses IncSearch label for highlighting
 -- labels, change label to bright green to make it more readable
 vim.api.nvim_set_hl(0, 'IncSearch', {
-  fg = '#f364b5', -- eldritch red
+  fg = global_colors.colors.eldritch_red, -- eldritch red
   bg = 'NONE', -- transparent background
   bold = true,
 })
@@ -257,10 +257,8 @@ vim.api.nvim_set_hl(0, 'IncSearch', {
 -- NOTE: original IncSearch fg and bg
 -- :hi IncSearch
 -- IncSearch      xxx guifg=#1a1c2c guibg=#f7c67f
-local fg_original = '#1a1c2c'
-local bg_original = '#f7c67f'
 vim.api.nvim_set_hl(0, 'YankHighlight', {
-  fg = fg_original, -- or 'NONE;
-  bg = bg_original, -- or 'NONE'
+  fg = global_colors.colors.fg_original, -- or 'NONE;
+  bg = global_colors.colors.bg_original, -- or 'NONE'
   bold = true, -- optional
 })
