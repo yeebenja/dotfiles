7fc8877af06c074af175339468ff97f4766dabd4
Benjamin Yee <yeebenja@outlook.com>
2025-12-26 19:42:51 -0800

    add ts and tsx config to nvim-ufo

--- .config/nvim/lua/nvim-ufo-config/nvim-fold-config.lua
+++ .config/nvim/lua/nvim-ufo-config/nvim-fold-config.lua
@@ -51,6 +51,8 @@ local ftMap = {
   vim = 'indent',
   python = { 'indent' },
   git = '',
+  typescript = { 'treesitter', 'indent' }, -- TS
+  typescriptreact = { 'treesitter', 'indent' }, -- TSX
 }
 require('ufo').setup {
 
