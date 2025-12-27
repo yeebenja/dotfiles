2eaefd71630a77e2ebd2bd502b2ad6345c316ef2
Benjamin Yee <benjaminyee@Bens-MacBook.local>
2025-12-26 19:46:32 -0800

    delete default keys for treesj

--- .config/nvim/lua/custom/plugins/tree-sj.lua
+++ .config/nvim/lua/custom/plugins/tree-sj.lua
@@ -1,5 +1,6 @@
 return {
   'Wansmer/treesj',
+  keys = {},
   dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
   config = function()
     require('treesj').setup {--[[ your config ]]
