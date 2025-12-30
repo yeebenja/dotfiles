37efc91cc1e5c28dfcc96c094d835a878b78eafc
Benjamin Yee <benjaminyee@Bens-MacBook.local>
2025-12-30 09:07:45 -0800

    modify conform.nvim to include toggle for formatting buffer on save

--- .config/nvim/lua/custom/plugins/conform.lua
+++ .config/nvim/lua/custom/plugins/conform.lua
@@ -7,14 +7,36 @@ return { -- Autoformat
       '<leader>f',
       function()
         require('conform').format { async = true, lsp_format = 'fallback' }
+        Snacks.notifier.notify('Formatted Buffer', 'info', { style = 'compact', timeout = 2000, title = 'Conform.nvim' })
+        print 'Formatted Buffer'
       end,
       mode = '',
       desc = '[F]ormat buffer',
+    },
+    {
+      '<leader>F',
+      function()
+        vim.g.disable_autoformat = not vim.g.disable_autoformat
+        if vim.g.disable_autoformat then
+          Snacks.notifier.notify('Conform: Format-on-save disabled', 'info', { style = 'compact', timeout = 2000, title = 'Conform.nvim' })
+          print 'Format-on-save disabled'
+        else
+          Snacks.notifier.notify('Conform: Format-on-save enabled', 'info', { style = 'compact', timeout = 2000, title = 'Conform.nvim' })
+          print 'Format-on-save enabled'
+        end
+      end,
+      mode = '',
+      desc = 'Toggle [F]ormat-on-save',
     },
   },
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
+      -- Check if autoformat is disabled globally
+      if vim.g.disable_autoformat then
+        return
+      end
+
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
