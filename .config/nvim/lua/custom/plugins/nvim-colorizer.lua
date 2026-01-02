ccf52fafadba012864492b214814b600207e01a8
Benjamin Yee <benjaminyee@macbookpro.attlocal.net>
2026-01-01 20:42:09 -0800

    use up-to-date and maintained repo for nvim.colorizer

--- .config/nvim/lua/custom/plugins/nvim-colorizer.lua
+++ .config/nvim/lua/custom/plugins/nvim-colorizer.lua
@@ -1,6 +1,8 @@
 return {
-  'norcalli/nvim-colorizer.lua',
+  'catgoose/nvim-colorizer.lua',
+  event = 'BufReadPre',
   config = function()
-    require('colorizer').setup {}
+    local colorizer = require 'colorizer'
+    colorizer.setup()
   end,
 }
