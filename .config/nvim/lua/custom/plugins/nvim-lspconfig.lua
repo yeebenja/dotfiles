68a65bc4532fd92639f5cb826b99fa4157fbd9ac
Benjamin Yee <benjaminyee@Bens-MacBook.local>
2026-01-01 19:14:51 -0800

    add language server dependencies

--- .config/nvim/lua/custom/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/custom/plugins/nvim-lspconfig.lua
@@ -206,6 +206,10 @@ return {
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
+      'lua-language-server',
+      'markdownlint',
+      'typescript-language-server',
+      'pyright',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
