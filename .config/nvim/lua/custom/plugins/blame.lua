07726b1f9204ec77bc318fb669873f270b8267b9
Benjamin Yee <benjaminyee@Bens-MacBook.local>
2026-01-03 13:22:34 -0800

    modify date in git blame and archive git-blame

--- .config/nvim/lua/custom/plugins/blame.lua
+++ .config/nvim/lua/custom/plugins/blame.lua
@@ -3,7 +3,9 @@ return {
     'FabijanZulj/blame.nvim',
     lazy = false,
     config = function()
-      require('blame').setup {}
+      require('blame').setup {
+        date_format = '%m.%d.%Y',
+      }
     end,
   },
 }
