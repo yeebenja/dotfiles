8bca8d48bb6f7ec6530cf1909aa44232410bfd97
Benjamin Yee <benjaminyee@Bens-MacBook.local>
2026-01-05 16:38:54 -0800

    move kickstart keybindings to kickstart-keybindings.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,21 +3,6 @@ _G.global_colors = require 'globals.colors' -- _G signifies that this table is i
 
 require 'vim-opts.vim-opts'
 
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -102,8 +87,7 @@ require('lazy').setup({
 
 -- Import keybindings
 require 'keybindings.keybindings'
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+require 'keybindings.kickstart-keybindings'
 vim.cmd 'colorscheme eldritch'
 
 -- Import fold
