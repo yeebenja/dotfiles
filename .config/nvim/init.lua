a120da35094c8fba3b8922c36f07109aeba6c445
Benjamin Yee <yeebenja@outlook.com>
2026-01-05 16:39:43 -0800

    clean up lazy.nvim comments

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,6 @@
 -- Ben's init.lua file for Neovim
-_G.global_colors = require 'globals.colors' -- _G signifies that this table is intentionally global
-
 require 'vim-opts.vim-opts'
+_G.global_colors = require 'globals.colors' -- _G signifies that this table is intentionally global
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
@@ -15,59 +14,13 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- Load themes
-  -- unpack(require 'themes.themes'),
   require 'themes.themes',
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
-  --
-  -- Use `opts = {}` to force a plugin to be loaded.
-  --
-  -- This is often very useful to both group configuration, as well as handle
-  -- lazy loading plugins that don't need to be loaded immediately at startup.
-  --
-  -- For example, in the following configuration, we use:
-  --  event = 'VimEnter'
-  --
-  -- which loads which-key before all the UI elements are loaded. Events can be
-  -- normal autocommands events (`:help autocmd-events`).
-  --
-  -- Then, because we use the `config` key, the configuration only runs
-  -- after the plugin has been loaded:
-  --  config = function() ... end
-
-  -- NOTE: Plugins can specify dependencies.
-  --
-  -- The dependencies are proper plugin specifications as well - anything
-  -- you do for a plugin at the top level, you can do for a dependency.
-  --
-  -- Use the `dependencies` key to specify the dependencies of a particular plugin
-
-  -- LSP Plugins
   { import = 'custom.plugins' },
-  -- NOTE: Here are custom keymaps for my plugins:
-  -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-  -- Or use telescope!
-  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-  -- you can continue same window with `<space>sr` which resumes last telescope search
 }, {
   ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
     icons = vim.g.have_nerd_font and {} or {
+      -- if not using nerd font: fallback on these icons
       cmd = '⌘',
       config = '🛠',
       event = '📅',
@@ -88,6 +41,8 @@ require('lazy').setup({
 -- Import keybindings
 require 'keybindings.keybindings'
 require 'keybindings.kickstart-keybindings'
+
+-- load colorscheme
 vim.cmd 'colorscheme eldritch'
 
 -- Import fold
