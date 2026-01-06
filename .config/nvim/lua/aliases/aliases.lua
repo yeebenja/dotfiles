69d212e02993efab3435e663f0ab6a2e33d997fe
Benjamin Yee <benjaminyee@Bens-MacBook.local>
2026-01-05 16:20:39 -0800

    move aliases to separate folder

---
+++ .config/nvim/lua/aliases/aliases.lua
@@ -0,0 +1,25 @@
+-- add leet command abbreviation
+vim.cmd [[
+  cnoreabbrev leet Leet
+]]
+
+-- add lazy command abbreviation
+vim.cmd [[
+  cnoreabbrev lazy Lazy
+]]
+
+-- DiffviewOpen and DiffviewClose
+vim.cmd [[
+  cnoreabbrev diffviewopen DiffviewOpen
+]]
+vim.cmd [[
+  cnoreabbrev diffviewclose DiffviewClose
+]]
+
+-- GrugFar and GrugFarWithin
+vim.cmd [[
+  cnoreabbrev grugfar GrugFar
+]]
+vim.cmd [[
+  cnoreabbrev grugfarwithin GrugFarWithin
+]]
