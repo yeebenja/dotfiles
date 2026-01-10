0905b392387f5f8a52fa3eed186d01b3c557b5bd
Benjamin Yee <yeebenja@outlook.com>
2026-01-09 23:49:32 -0600

    add descriptions for keymaps

--- .config/nvim/lua/user-commands/user-commands.lua
+++ .config/nvim/lua/user-commands/user-commands.lua
@@ -3,4 +3,4 @@ vim.api.nvim_create_user_command('Messages', function()
   vim.cmd 'enew'
   vim.api.nvim_put(vim.split(vim.fn.execute 'messages', '\n'), 'l', true, false)
   vim.bo.buftype = 'nofile'
-end, {})
+end, { desc = 'Load Messages in Buffer' })
