bff81864a1dac61344e6caafb4a5f6547389d592
Benjamin Yee <benjaminyee@Bens-MacBook.local>
2026-01-10 02:27:06 -0600

    add markdown-preview

---
+++ .config/nvim/lua/custom/plugins/markdown-preview.lua
@@ -0,0 +1,10 @@
+return {
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  -- install with yarn or npm
+  build = 'cd app && yarn install',
+  init = function()
+    vim.g.mkdp_filetypes = { 'markdown' }
+  end,
+  ft = { 'markdown' },
+}
