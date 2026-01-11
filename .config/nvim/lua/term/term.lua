ec88344d6eee4e046011ae8b11648eca7ddbf36b
Benjamin Yee <benjaminyee@Bens-MacBook.local>
2026-01-11 01:32:11 -0600

    add ls -a and git status commands for nvim terminal

--- .config/nvim/lua/term/term.lua
+++ .config/nvim/lua/term/term.lua
@@ -8,14 +8,23 @@ vim.keymap.set('n', '<leader>N', function()
   last_term_bufnr = vim.api.nvim_get_current_buf()
 end, { desc = '[N]ew Terminal' })
 
--- send commands to most recent terminal
-vim.keymap.set('n', '<leader>m1', function()
-  vim.fn.chansend(last_job_id, { 'ls -al\r\n' })
-
-  -- Scroll terminal to bottom
+-- EFFECTS: Scrolls terminal buffer to bottom
+local scroll_to_bottom = function()
   for _, win in ipairs(vim.api.nvim_list_wins()) do
     if vim.api.nvim_win_get_buf(win) == last_term_bufnr then
       vim.api.nvim_win_set_cursor(win, { vim.api.nvim_buf_line_count(last_term_bufnr), 0 })
     end
   end
-end)
+end
+
+-- ls -a
+vim.keymap.set('n', '<leader>ml', function()
+  vim.fn.chansend(last_job_id, { 'ls -a\r\n' })
+  vim.schedule(scroll_to_bottom)
+end, { desc = 'ls -a' })
+
+-- git status
+vim.keymap.set('n', '<leader>mg', function()
+  vim.fn.chansend(last_job_id, { 'git status\r\n' })
+  vim.schedule(scroll_to_bottom)
+end, { desc = 'git status' })
