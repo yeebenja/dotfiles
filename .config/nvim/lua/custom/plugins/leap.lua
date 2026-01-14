aa6f6c0f8a2895812addb2e5536cd5e8aba3424f
Benjamin Yee <benjaminyee@Bens-MacBook.local>
2026-01-14 00:48:41 -0600

    fix highlight groups for leap.nvim and / search in vim

--- .config/nvim/lua/custom/plugins/leap.lua
+++ .config/nvim/lua/custom/plugins/leap.lua
@@ -3,5 +3,57 @@ return {
   config = function()
     local leap = require 'leap'
     leap.opts.case_sensitive = false -- faster to leap when case sensitivity is False
+
+    -- store original IncSearch highlight
+    local original_incsearch = nil
+
+    -- autocommand group
+    local leap_group = vim.api.nvim_create_augroup('LeapHighlightTweaks', { clear = true })
+
+    -- autocommand for when User enters leap.nvim
+    -- EFFECTS: sets IncSearch to be our own custom color
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'LeapEnter',
+      group = leap_group,
+      callback = function()
+        -- store original IncSearch highlight bejore changing it
+        local hl = vim.api.nvim_get_hl(0, { name = 'IncSearch' })
+        original_incsearch = hl
+
+        -- then set custom IncSearch color
+        vim.api.nvim_set_hl(0, 'IncSearch', {
+          fg = global_colors.colors.eldritch_red,
+          bg = 'NONE',
+          bold = true,
+        })
+      end,
+    })
+
+    -- autocommand for when User leavers leap.nvim
+    -- EFFECTS: set IncSearch back to original color
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'LeapLeave',
+      group = leap_group,
+      callback = function()
+        -- restore original IncSearch highlight
+        if original_incsearch then
+          vim.api.nvim_set_hl(0, 'IncSearch', original_incsearch)
+        end
+      end,
+    })
+
+    -- EFFECTS: sets search area as 'Comment' color
+    local function set_leap_backdrop()
+      vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
+    end
+
+    set_leap_backdrop()
+
+    -- autocommand to reapply search area highlight group whenever
+    -- colorscheme changes
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      group = leap_group,
+      callback = set_leap_backdrop,
+    })
   end,
 }
--- .config/nvim/lua/highlight-groups/highlight-groups.lua
+++ .config/nvim/lua/highlight-groups/highlight-groups.lua
@@ -1,17 +1,19 @@
 -- highlight-groups.lua
--- NOTE: since leap.nvim uses IncSearch label for highlighting
--- labels, change label to bright green to make it more readable
-vim.api.nvim_set_hl(0, 'IncSearch', {
-  fg = global_colors.colors.eldritch_red, -- eldritch red
-  bg = 'NONE', -- transparent background
-  bold = true,
-})
 
 -- NOTE: original IncSearch fg and bg
 -- :hi IncSearch
 -- IncSearch      xxx guifg=#1a1c2c guibg=#f7c67f
-vim.api.nvim_set_hl(0, 'YankHighlight', {
-  fg = global_colors.colors.fg_original, -- or 'NONE;
-  bg = global_colors.colors.bg_original, -- or 'NONE'
-  bold = true, -- optional
+
+local function reapply_hl()
+  vim.api.nvim_set_hl(0, 'YankHighlight', {
+    fg = global_colors.colors.fg_original, -- or 'NONE'
+    bg = global_colors.colors.bg_original, -- or 'NONE'
+    bold = true,
+  })
+end
+
+reapply_hl()
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  callback = reapply_hl,
 })
