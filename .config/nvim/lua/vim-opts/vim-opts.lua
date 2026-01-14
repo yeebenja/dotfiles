22f10730df29ec05f595822efe65665eb6d293db
Benjamin Yee <benjaminyee@MacBookPro.localdomain>
2026-01-14 12:01:05 -0600

    fix disable auto-format to be applied to all buffers (globally)

--- .config/nvim/lua/vim-opts/vim-opts.lua
+++ .config/nvim/lua/vim-opts/vim-opts.lua
@@ -64,3 +64,6 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+-- keey auto format on
+vim.g.disable_autoformat = false
