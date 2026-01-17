b2716800c2f5b2c71067a7f56402231794a01673
Benjamin Yee <benjaminyee@Bens-MacBook.local>
2026-01-16 23:58:58 -0600

    add scratch buffer keybindings

---
+++ .config/nvim/lua/custom/plugins/scratch.lua
@@ -0,0 +1,52 @@
+-- https://github.com/LintaoAmons/scratch.nvim
+return {
+  'LintaoAmons/scratch.nvim',
+  event = 'VeryLazy',
+  dependencies = {
+    { 'folke/snacks.nvim' }, -- using snacks picker
+  },
+  config = function()
+    local scratch = require 'scratch'
+    scratch.setup {
+      scratch_file_dir = vim.fn.stdpath 'cache' .. '/scratch.nvim', -- where your scratch files will be put
+      window_cmd = 'rightbelow vsplit', -- 'vsplit' | 'split' | 'edit' | 'tabedit' | 'rightbelow vsplit'
+      file_picker = 'snacks',
+      filetypes = { 'md', 'txt', 'lua', 'js', 'sh', 'ts' },
+      filetype_details = { -- or, you can have more control here
+        -- json = {}, -- empty table is fine
+        -- ['project-name.md'] = {
+        --   subdir = 'project-name', -- group scratch files under specific sub folder
+        -- },
+        -- ['yaml'] = {},
+        -- go = {
+        --   requireDir = true, -- true if each scratch file requires a new directory
+        --   filename = 'main', -- the filename of the scratch file in the new directory
+        --   content = { 'package main', '', 'func main() {', '  ', '}' },
+        --   cursor = {
+        --     location = { 4, 2 },
+        --     insert_mode = true,
+        --   },
+        -- },
+      },
+      localKeys = {
+        -- {
+        --   filenameContains = { 'sh' },
+        --   LocalKeys = {
+        --     {
+        --       cmd = '<CMD>RunShellCurrentLine<CR>',
+        --       key = '<C-r>',
+        --       modes = { 'n', 'i', 'v' },
+        --     },
+        --   },
+        -- },
+      },
+      hooks = {
+        -- {
+        --   callback = function()
+        --     vim.api.nvim_buf_set_lines(0, 0, -1, false, { 'hello', 'world' })
+        --   end,
+        -- },
+      },
+    }
+  end,
+}
