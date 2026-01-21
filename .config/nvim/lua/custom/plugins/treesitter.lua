deb3b40fb28fc7e959c4318de0c02be298e2c46a
Benjamin Yee <benjaminyee@Bens-MacBook.local>
2026-01-21 01:29:42 -0600

    add missing languages to treesitter

--- .config/nvim/lua/custom/plugins/treesitter.lua
+++ .config/nvim/lua/custom/plugins/treesitter.lua
@@ -9,7 +9,25 @@ return {
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+    ensure_installed = {
+      'bash',
+      'c',
+      'diff',
+      'lua',
+      'luadoc',
+      'markdown',
+      'markdown_inline',
+      'query',
+      'vim',
+      'vimdoc',
+      'json',
+      'html',
+      'css',
+      'scss',
+      'javascript',
+      'typescript',
+      'tsx',
+    },
     -- Autoinstall languages that are not installed
     auto_install = true,
     highlight = {
