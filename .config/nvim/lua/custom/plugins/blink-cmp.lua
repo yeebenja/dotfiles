e6c7fc030232bfbc41420f6a4704ea0378fd2f9e
Benjamin Yee <benjaminyee@Bens-MacBook.local>
2026-01-23 00:51:40 -0600

    add colorful-menu to blink-cmp

--- .config/nvim/lua/custom/plugins/blink-cmp.lua
+++ .config/nvim/lua/custom/plugins/blink-cmp.lua
@@ -3,7 +3,9 @@
 return {
   'saghen/blink.cmp',
   event = { 'InsertEnter', 'CmdlineEnter' }, -- Load when entering insert mode or entering cmdline
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  dependencies = {
+    'xzbdmw/colorful-menu.nvim',
+  },
   version = '1.*',
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
@@ -26,6 +28,8 @@ return {
       ['<C-j>'] = { 'snippet_backward', 'fallback' },
 
       ['<C-0>'] = { 'show_signature', 'hide_signature', 'fallback' },
+      -- ['<C-u>'] = { 'scroll_signature_up', 'fallback' },
+      -- ['<C-d>'] = { 'scroll_signature_down', 'fallback' },
     },
 
     appearance = {
@@ -38,6 +42,23 @@ return {
       -- auto brackets
       -- NOTE: some LSPs may add auto brackets themselves anyway
       accept = { auto_brackets = { enabled = true } },
+      menu = {
+        draw = {
+          -- We don't need label_description now because label and label_description are already
+          -- combined together in label by colorful-menu.nvim.
+          columns = { { 'kind_icon' }, { 'label', gap = 1 } },
+          components = {
+            label = {
+              text = function(ctx)
+                return require('colorful-menu').blink_components_text(ctx)
+              end,
+              highlight = function(ctx)
+                return require('colorful-menu').blink_components_highlight(ctx)
+              end,
+            },
+          },
+        },
+      },
     }, -- disable if this gets annoying
 
     -- Default list of enabled providers defined so that you can extend it
