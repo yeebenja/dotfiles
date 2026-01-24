020b96a65c9b64763afaafbb5b6e7a0f42f89376
Benjamin Yee <yeebenja@outlook.com>
2026-01-23 19:56:26 -0600

    modify lualine statusline

--- .config/nvim/lua/custom/plugins/lualine.lua
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -4,18 +4,14 @@ local function git_worktree_with_icon()
   if not handle then
     return ''
   end
-
   local result = handle:read '*a'
   handle:close()
-
   if not result or result == '' then
     return ''
   end
-
   local worktree = result:gsub('\n', '')
   local name = worktree:match '[^/]+$'
   local icon = '󱁕' -- (git worktree icon)
-
   return string.format('%s %s', icon, name)
 end
 
@@ -41,8 +37,56 @@ local function modified_buffers_count()
   return string.format('● %d', modified_buffers)
 end
 
-return {
+-- EFFECTS: creates custom theme from current highlight groups
+local function get_custom_theme()
+  local function get_hl(name)
+    local hl = vim.api.nvim_get_hl(0, { name = name, link = false })
+    return {
+      fg = hl.fg and string.format('#%06x', hl.fg) or nil,
+      bg = hl.bg and string.format('#%06x', hl.bg) or nil,
+      gui = hl.bold and 'bold' or hl.italic and 'italic' or nil,
+    }
+  end
 
+  local normal = get_hl 'Normal'
+  local statusline = get_hl 'StatusLine'
+  local statusline_nc = get_hl 'StatusLineNC'
+  local mode_normal = get_hl '@variable'
+  local mode_insert = get_hl '@keyword'
+  local mode_visual = get_hl '@keyword.function'
+  local mode_replace = get_hl 'Constant'
+  local mode_command = get_hl 'Function'
+  local diff_add = get_hl 'DiffAdd'
+  local diff_change = get_hl 'DiffChange'
+  local diff_delete = get_hl 'DiffDelete'
+
+  return {
+    normal = {
+      a = { fg = statusline.bg or normal.bg, bg = mode_normal.fg, gui = 'bold' },
+      b = { fg = statusline.fg, bg = statusline.bg },
+      c = { fg = statusline.fg, bg = statusline.bg },
+    },
+    insert = {
+      a = { fg = statusline.bg or normal.bg, bg = mode_insert.fg, gui = 'bold' },
+    },
+    visual = {
+      a = { fg = statusline.bg or normal.bg, bg = mode_visual.fg, gui = 'bold' },
+    },
+    replace = {
+      a = { fg = statusline.bg or normal.bg, bg = mode_replace.fg, gui = 'bold' },
+    },
+    command = {
+      a = { fg = statusline.bg or normal.bg, bg = mode_command.fg, gui = 'bold' },
+    },
+    inactive = {
+      a = { fg = statusline_nc.fg, bg = statusline_nc.bg },
+      b = { fg = statusline_nc.fg, bg = statusline_nc.bg },
+      c = { fg = statusline_nc.fg, bg = statusline_nc.bg },
+    },
+  }
+end
+
+return {
   'nvim-lualine/lualine.nvim',
   event = 'VimEnter',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -50,75 +94,90 @@ return {
   -- startup because it looks cleaner
   --
   -- event = ...
-  opts = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    always_show_tabline = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 100,
-      tabline = 100,
-      winbar = 100,
-      refresh_time = 16, -- ~60fps
-    },
-    sections = {
-      lualine_a = { 'mode' },
-      lualine_b = {
-        'branch',
-        -- show current git worktree
-        {
-          git_worktree_with_icon,
-          cond = function()
-            return vim.fn.isdirectory '.git' == 1 or vim.fn.system('git rev-parse --git-dir 2>/dev/null'):find '.git' ~= nil
-          end,
+  config = function()
+    local function load_lualine()
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          theme = get_custom_theme(),
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          always_show_tabline = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 100,
+            tabline = 100,
+            winbar = 100,
+            refresh_time = 16, -- ~60fps
+          },
         },
-        'diff',
-        'diagnostics',
-      },
-      lualine_c = {
-        'filename',
-        -- -- date time
-        -- {
-        --   function()
-        --     return os.date '%H:%M:%S %a %m/%d/%Y'
-        --   end,
-        -- }, -- 14:30 12/24/2024
-        -- Git worktree
-      },
-      lualine_x = {
-        {
-          modified_buffers_count,
-          color = { fg = global_colors.colors.modified_buffer_color },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = {
+            'branch',
+            -- show current git worktree
+            {
+              git_worktree_with_icon,
+              cond = function()
+                return vim.fn.isdirectory '.git' == 1 or vim.fn.system('git rev-parse --git-dir 2>/dev/null'):find '.git' ~= nil
+              end,
+            },
+            'diff',
+            'diagnostics',
+          },
+          lualine_c = {
+            'filename',
+          },
+          lualine_x = {
+            {
+              modified_buffers_count,
+              color = { fg = global_colors.colors.modified_buffer_color },
+            },
+            {
+              format_on_save_status,
+            },
+            'encoding',
+            'fileformat',
+            'filetype',
+          },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
         },
-        {
-          format_on_save_status,
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {},
         },
-        'encoding',
-        'fileformat',
-        'filetype',
-      },
-      lualine_y = { 'progress' },
-      lualine_z = { 'location' },
-    },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = { 'filename' },
-      lualine_x = { 'location' },
-      lualine_y = {},
-      lualine_z = {},
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {},
-  },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {},
+      }
+    end
+
+    load_lualine()
+
+    -- EFFECTS: autocmd to refresh lualine when colorscheme changes
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      pattern = '*',
+      callback = function()
+        load_lualine()
+      end,
+      desc = 'Refresh lualine theme when colorscheme changes',
+    })
+
+    -- EFFECTS: refresh function globally for manual reload
+    vim.api.nvim_create_user_command('LualineRefresh', function()
+      load_lualine()
+    end, { desc = 'Manually refresh lualine theme' })
+  end,
 }
