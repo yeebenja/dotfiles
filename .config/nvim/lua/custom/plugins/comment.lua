9e70868f55cf301ab736e4dce53a75507e0481e0
Benjamin Yee <yeebenja@outlook.com>
2026-01-24 00:54:50 -0600

    add comment support for jsx/tsx

--- .config/nvim/lua/custom/plugins/comment.lua
+++ .config/nvim/lua/custom/plugins/comment.lua
@@ -1,7 +1,24 @@
 return {
   'numToStr/Comment.nvim',
   event = { 'BufReadPost', 'BufNewFile' },
+  dependencies = {
+    -- for jsx/tsx commenting
+    {
+      'JoosepAlviste/nvim-ts-context-commentstring',
+      config = function()
+        require('ts_context_commentstring').setup {
+          enable_autocmd = false,
+        }
+      end,
+    },
+  },
   opts = {
     -- add any options here
   },
+  config = function()
+    local comment = require 'Comment'
+    comment.setup {
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+    }
+  end,
 }
