575d59afc7ee47b8482717a1f173e55ed912195e
Benjamin Yee <benjaminyee@Bens-MacBook.local>
2026-02-12 16:34:02 -0600

    increase conform timeout

--- .config/nvim/lua/custom/plugins/conform.lua
+++ .config/nvim/lua/custom/plugins/conform.lua
@@ -21,7 +21,7 @@ return { -- Autoformat
         lsp_format_opt = 'fallback'
       end
       return {
-        timeout_ms = 1000,
+        timeout_ms = 5000,
         lsp_format = lsp_format_opt,
       }
     end,
