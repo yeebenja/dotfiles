bd2cefdc25e8394abc54a950655c46a371fffaab
Benjamin Yee <yeebenja@outlook.com>
2026-04-05 14:56:14 -0500

    disable ts-autotag

---
+++ .config/nvim/lua/custom/plugins/DISABLED_ts-autotag.lua
@@ -0,0 +1,25 @@
+return {
+  -- NOTE: wait for nvim-ts-autotag to be updated to work with main
+  -- branch of treesitter
+  --
+  -- 'windwp/nvim-ts-autotag',
+  -- event = { 'BufReadPre', 'BufNewFile' },
+  -- config = function()
+  --   local autotag = require 'nvim-ts-autotag'
+  --   autotag.setup {
+  --     opts = {
+  --       enable_close = true, -- Auto close tags
+  --       enable_rename = true, -- Auto rename pairs of tags
+  --       enable_close_on_slash = false, -- Auto close on trailing </
+  --     },
+  --     -- Also override individual filetype configs, these take priority.
+  --     -- Empty by default, useful if one of the "opts" global settings
+  --     -- doesn't work well in a specific filetype
+  --     per_filetype = {
+  --       ['html'] = {
+  --         enable_close = false,
+  --       },
+  --     },
+  --   }
+  -- end,
+}
