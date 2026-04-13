8b2f9576b2473485b455173ab9baf82e6c4fdd8b
Benjamin Yee <yeebenja@outlook.com>
2026-04-13 11:00:56 -0500

    change blink-cmp to use 'prefer_rust'

--- .config/nvim/lua/custom/plugins/blink-cmp.lua
+++ .config/nvim/lua/custom/plugins/blink-cmp.lua
@@ -74,7 +74,7 @@ return {
     -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
     --
     -- See the fuzzy documentation for more information
-    fuzzy = { implementation = 'prefer_rust_with_warning' },
+    fuzzy = { implementation = 'prefer_rust' },
 
     -- See cmdline documentation:
     -- https://cmp.saghen.dev/modes/cmdline
