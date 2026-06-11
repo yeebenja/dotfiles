675891f98b41a1b50e312f55a01a83bc3274a6f7
Benjamin Yee <yeebenja@outlook.com>
2026-06-10 21:53:06 -0500

    fix: delete overlapping keymap

--- .config/nvim/lua/custom/plugins/gitsigns.lua
+++ .config/nvim/lua/custom/plugins/gitsigns.lua
@@ -104,7 +104,7 @@ return {
       end, { desc = 'git [D]iff against last commit' })
       -- Toggles
       -- map('n', '<leader>gb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
-      map('n', '<leader>gD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
+      -- map('n', '<leader>gD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
     end,
   },
 }
