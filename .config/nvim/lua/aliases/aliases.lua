169664b31cf64db4b61290ff96175b950269f9e6
Benjamin Yee <yeebenja@outlook.com>
2026-06-17 20:56:08 -0500

    chore(nvim): remove videre.nvim, clean up aliases, add mason alias

--- .config/nvim/lua/aliases/aliases.lua
+++ .config/nvim/lua/aliases/aliases.lua
@@ -1,23 +1,18 @@
--- add leet command abbreviation
+-- aliases.lua
+
 vim.cmd [[
   cnoreabbrev leet Leet
 ]]
 
--- add lazy command abbreviation
 vim.cmd [[
   cnoreabbrev lazy Lazy
 ]]
 
--- DiffviewOpen and DiffviewClose
 vim.cmd [[
   cnoreabbrev diffviewopen DiffviewOpen
   cnoreabbrev dvo DiffviewOpen
   cnoreabbrev diffviewclose DiffviewClose
   cnoreabbrev dvc DiffviewClose
-]]
-
-vim.cmd [[
-  cnoreabbrev json Videre
 ]]
 
 vim.cmd [[
